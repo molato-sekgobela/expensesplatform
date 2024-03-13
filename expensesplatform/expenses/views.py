419ba2fa0dff245f43aeabecf0002b443fb7f53f
Molato Sekgobela (Santam) <Molato.Sekgobela@santam.co.za>
2024-03-13 17:27:46 +0200

    add add-expense functionality

--- expensesplatform/expenses/views.py
+++ expensesplatform/expenses/views.py
@@ -1,14 +1,41 @@
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
+from .models import Expense, Category
+from django.contrib import messages
 
 @login_required(login_url='/authentication/login')
 def index(request):
-    return render(request, 'expenses/index.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+    }
+    return render(request, 'expenses/index.html', context)
 
 @login_required(login_url='/authentication/login')
 def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
+    
+    categories = Category.objects.all()
 
+    context = {
+        'categories': categories,
+        'values': request.POST
+    }
+    if request.method == 'GET':
+        
+        return render(request, 'expenses/add_expense.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+        
+        
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/add_expense.html', context)
+        
+        return render(request, 'expenses/add_expense.html', context)
+
+        #Expense.objects.create(amount=amount, date=date, category=category, description=description)
+    
 @login_required(login_url='/authentication/login')
 def add_income(request):
     return render(request,'expenses/income.html')
